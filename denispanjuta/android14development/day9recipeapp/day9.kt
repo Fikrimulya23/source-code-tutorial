db18f9597c71aefdc20a4862c11b08a3883ce1e6
Fikri Mulya Permana <fikrimulyapermana00@gmail.com>
2024-03-28 08:47:15 +0700

    add new tutorial

---
+++ denispanjuta/android14development/day9recipeapp/day9.kt
@@ -0,0 +1,44 @@
+/*
+* view model
+* implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+* retrofit
+* implementation("com.squareup.retrofit2:retrofit:2.10.0")
+* convert to gson
+* implementation("com.squareup.retrofit2:converter-gson:2.10.0")
+* image loading
+* implementation("io.coil-kt:coil-compose:2.6.0")*/
+
+/*
+
+* converter-gson digunakan untuk mengubah json object ke kotlin object
+* dengan begini, kita bisa langsung mengakses properti dari objek tersebut
+
+* Suspend Function Characteristics
+* - Non-Blocking
+* Tidak memblokir thread yang sedang berjalan, melainkan menangguhkan eksekusi coroutine.
+* Ketika hasilnya sudah siap, coroutine akan melanjutkan konteks coroutine.
+
+* - Coroutine Context
+* Fungsi penangguhan hanya dapat dipanggil dari coroutine atau fungsi penangguhan lainnya.
+* Mereka adalah inti dari pendekatan Kotlin untuk fleksibilitas asynchronous programming
+
+* - Flexibility
+* Sewaktu dijeda, tugas atau fungsi lainnya dapat berlajan, memanfaatkan sumber daya dengan lebih
+* baik dan meningkatkan daya tanggap, khususnya pada aplikasi dengan user interface*/
+
+// Try Catch
+fun main() {
+    try {
+        println("Please enter a number")
+        var number = readln().toInt()
+        println("Your number is $number")
+    } catch (e: Exception) {
+        println("Error: $e")
+    } finally {
+        println("This is Finally")
+    }
+}
+
+/*
+*
+* */
